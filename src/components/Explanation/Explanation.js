6bc7edd6e4ffea06d9fde6dda9d104be1963855a
Ant Man <34754995+aaronamendez@users.noreply.github.com>
2021-12-09 19:06:06 -0500

    Finished adding styled components.

--- src/components/Explanation/Explanation.js
+++ src/components/Explanation/Explanation.js
@@ -1,12 +1,26 @@
 import React from "react";
+import styled from "styled-components";
 import "./index.css";
+
+const ExplanationStyled = styled.div`
+	p {
+		font-size: 1.3em;
+		width: 100%;
+		text-align: center;
+	}
+
+	.explanation-wrapper {
+		max-width: 50%;
+		margin: 0 auto;
+	}
+`;
 
 const Explanation = (props) => {
 	const { text } = props;
 	return (
-		<div className="explanation-wrapper">
+		<ExplanationStyled className="explanation-wrapper">
 			<p>{text}</p>
-		</div>
+		</ExplanationStyled>
 	);
 };
 
